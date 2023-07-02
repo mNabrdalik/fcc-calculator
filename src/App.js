0e0c6cdff177cf48d2694c0158240b6884d8515e
mNabrdalik <m.nabrdalik.93@gmail.com>
2023-07-02 23:26:26 +0200

    done test 15/16

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import React from 'react';
+import {evaluate} from 'mathjs';
+// math
 
 function App() {
 
   const btnArr= [
     {
       id: "divide",
-      value: "/",
+      value: " / ",
       type: "operator"
     },
     {
       id: "multiply",
-      value: "x",
+      value: " * ",
       type: "operator"
     },
     {
@@ -31,7 +33,7 @@ function App() {
     },
     {
       id: "subtract",
-      value: "-",
+      value: " - ",
       type: "operator"
     },
     {
@@ -51,7 +53,7 @@ function App() {
     },
     {
       id: "add",
-      value: "+",
+      value: " + ",
       type: "operator"
     },
     {
@@ -71,13 +73,13 @@ function App() {
     },
     {
       id: "equals",
-      value: "=",
-      type: "operator"
+      value: " = ",
+      type: ""
     },
     {
       id: "decimal",
       value: ".",
-      type: "operator"
+      type: ""
     },
     {
       id: "zero",
@@ -86,7 +88,7 @@ function App() {
     },
   ]
 
-  const[history, setHistory] = React.useState("")
+  const[calculation, setCalculation] = React.useState("")
   const[output, setOutput] = React.useState("0")
 
   const btnValues = btnArr.map((item, i) => {
@@ -97,25 +99,75 @@ function App() {
 
   function handleClick(event) {
     const btnClicked = event.target
-    console.log(btnClicked.className)
-    if(btnClicked.className === "num") {
-      if(output == "0") {
-        setOutput(btnClicked.value)
-      } else {
-        setOutput(preVal => preVal + btnClicked.value);
-      }
-    }
   
+    if(btnClicked.className !== "operator") {
+      if(output == "0" || isNaN(output)) {
+        setOutput(btnClicked.value)
+      } else if(btnClicked.id !== "equals" && btnClicked.id !== "decimal") {
+        setOutput(preVal => preVal + btnClicked.value)
+      }
+  
+      
+      if(btnClicked.id === "decimal") {
+        if(!output.includes(".")) {
+          setOutput(preVal => preVal + btnClicked.value)
+          // setCalculation(preCalc => preCalc + btnClicked.value)
+        }
+  
+      } 
+    
+      if(btnClicked.id === "equals") {
+      
+        let equals = evaluate(calculation)
+        setOutput(equals)
+        setCalculation(preCalc => preCalc + btnClicked.value + equals)
+  
+      } else {
+        setCalculation(preCalc => preCalc + btnClicked.value)
+      }
+     
+    } else if(btnClicked.className === "operator"){
+
+      if(calculation.indexOf("=") !== -1) {
+        setCalculation(preCalc => preCalc.slice(calculation.indexOf("=") + 1))
+      }
+
+      setOutput(btnClicked.value)
+      setCalculation(preCalc => preCalc + btnClicked.value)
+
+      // to fix!!!
+      if(
+        calculation[calculation.length - 2] === "+" || 
+        calculation[calculation.length - 2] === "-")  {
+          let len = calculation.length - 2
+          let newCalc = calculation.substring(0, len) + calculation.substring(len + 1,)
+  
+          // console.log(newCalc );
+          setCalculation(newCalc + btnClicked.value)
+        }
+
+
+
+    }
+    
+
+
   }
 
+
   function clearScreen() {
-    setHistory("")
-    setOutput(0)
+    setCalculation("")
+    setOutput("0")
   }
+
+  // React.useEffect(() => {
+  //   console.log(calculation);
+  //   console.log("pos: " + calculation[calculation.length - 5]);
+  // }, [calculation]);
 
   return (
     <div className="App">
-      <div className="history">{history}</div>
+      <div className="calculation">{calculation}</div>
       <div id="display">{output}</div>
       <button id="clear" value="AC" onClick={clearScreen}>AC</button>
 
